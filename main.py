565ed628f9e20b79e6f8270bbf226c5973dd7e61
Douglas Henrique Custodio Hotz <88330413+Douglas29X@users.noreply.github.com>
2021-09-25 17:14:39 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+
+numero_de_pessoas = (int(input
+	('Digite o número de pessoas que estão na sala nesse exato momento:\n')))
+
+
+def calcula_aniversario(n):
+	chance = (1/365) ** n
+	for fatorial in range((366 - n), 366):
+		chance*= fatorial
+	return (1 - chance) * 100
+
+calculo = calcula_aniversario(numero_de_pessoas)
+
+print('\n\nA chance de duas pessoas, nessa sala, fazerem'
+	f' aniversário no mesmo dia é de {calculo:.2f}%')
+
+print(f'Lembrando que esse é o resultado aproximado.\n\nA probabilidade original é de {calculo}%\n')
